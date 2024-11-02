1c83a13da154e058fd36c06d382fcd893753daba
cyclizar <pock01@yahoo.com>
2024-11-02 03:35:19 -0400

    added percentage to cmd v6

--- src/core/file_format/pkg.cpp
+++ src/core/file_format/pkg.cpp
@@ -136,24 +136,29 @@ bool PKG::Extract(const std::filesystem::path& filepath, const std::filesystem::
         return totalSize;
     };
     new std::thread([&] {
-        auto folderSizePrint = [&]() {
-            pkgSize = file.GetSize();
-            while (true) {
-                std::this_thread::sleep_for(std::chrono::seconds(1));
-                uintmax_t size = getFolderSize(extract_path);
-                if (pkgSize != 0) {
-                    auto pkgExtractionPercentageVariable1 = size / pkgSize;
+        pkgSize = file.GetSize();
+        while (true) {
+            std::this_thread::sleep_for(std::chrono::seconds(1));
+            uintmax_t size = getFolderSize(extract_path);
+
+            if (pkgSize != 0) {
+                auto pkgExtractionPercentageVariable1 = static_cast<double>(size) / pkgSize;
+                if (pkgExtractionPercentageVariable1 != 0) { // Ensure it's not zero
                     auto pkgExtractionPercentage = 100 / pkgExtractionPercentageVariable1;
                     std::cout << "Size of the folder: " << size << " bytes, " << pkgExtractionPercentage << "%" << std::endl;
+                } else {
+                    std::cout << "Size of the folder: " << size << " bytes, " << "0" << "%" << std::endl;
                 }
-                else {
-                   std::cout << "Size of the folder: " << size << " bytes, " << "0" << "%" << std::endl; 
-                }
-                    if (size >= pkgSize) {
-                        return 0;
-                    }
+            } else {
+                std::cout << "Size of the folder: " << size << " bytes, " << "0" << "%" << std::endl; 
             }
-        };
+
+            if (size >= pkgSize) {
+                break;  // Use break to exit the loop
+            }
+        }
+    }).detach();  // Detach the thread if you don’t need to join it
+
     
     folderSizePrint();
 
